b9c6a4d06e5186f6bf7994be15cfce975a6c0360
Igorth <igordiasth@gmail.com>
2024-01-31 11:50:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# everything to Know to use TypeScript
+
+Coming ...
+
+This project was created with ❤️ by [Igor Dias](https://igordiasth.dev).
---
+++ ts-number/index.js
@@ -0,0 +1,36 @@
+"use strict";
+// NUMBER
+////////////////////////////////
+// let x: number = 10;
+// console.log(x);
+// console.log(typeof x);
+// x = 'teste';
+// console.log(typeof x);
+// const y: number = 13.2355;
+// console.log(typeof y);
+// console.log(y);
+// console.log(y.toFixed(2));
+// console.log(y.toUpperCase());
+// STRING
+/////////////////////////////////
+// const firstName: string = 'John';
+// console.log(firstName.toUpperCase());
+// let fullName: string;
+// const lastName: string = 'Doe';
+// console.log(firstName + ' ' + lastName);
+// BOOLEAN
+/////////////////////////////////
+// let a: boolean = true;
+// console.log(a);
+// console.log(typeof a);
+// INFERENCE AND ANNOTATION
+// const ann: string = 'JSON';
+// let inf = 'TypeScript';
+// console.log(inf);
+// console.log(typeof inf);
+////////////////////////////////
+// Challenge 1
+const age = 25;
+const convString = age.toString();
+const message = `Olá ${age} anos `;
+console.log(message);
