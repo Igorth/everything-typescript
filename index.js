a9f2367aefe56771a189d0debd954f20ce204ef5
Igorth <igordiasth@gmail.com>
2024-01-31 10:27:44 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const firstName = 'John';
+// const lastName = 1;
+// const x = true;
+// function greetings(name: string) {
+//   console.log('Hello' + name);
+// }
+// greetings(firstName);
+// greetings(lastName);
+// greetings(x);
+function sumN(a, b) {
+    return a + b;
+}
+var result = sumN(1, 2);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,17 @@
+// const firstName = 'John';
+// const lastName = 1;
+// const x = true;
+
+// function greetings(name: string) {
+//   console.log('Hello' + name);
+// }
+
+// greetings(firstName);
+// greetings(lastName);
+// greetings(x);
+
+function sumN(a: number, b: number) {
+  return a + b;
+}
+const result = sumN(1, 2);
+console.log(result);
