e951cd2ef953b1a013a96bd3296f5fcc399aaff6
Igorth <igordiasth@gmail.com>
2024-02-08 12:26:40 -0500

    feat: add styling in the header, footer and global

--- ts-todolist/src/components/Footer.tsx
+++ ts-todolist/src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         <span>TypeScript and React</span> @ 2024
       </p>
