5d3108806c2c82ffe1b8adc05575a324e268609e
Igorth <igordiasth@gmail.com>
2024-02-11 07:08:21 -0500

    feat: configure mongoose with atlas as our DB

--- ts-api-restful/src/app.ts
+++ ts-api-restful/src/app.ts
@@ -1,1 +1,26 @@
-console.log('rodando');
+// ENV Variables
+require('dotenv').config();
+
+import express from 'express';
+import config from 'config';
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+// DB
+import db from '../config/db';
+
+// Routes
+import router from './router';
+
+app.use('/api', router);
+
+// APP port
+const port = config.get<number>('port');
+
+app.listen(3000, async () => {
+  await db();
+  console.log(`Server running on port ${port}`);
+});
