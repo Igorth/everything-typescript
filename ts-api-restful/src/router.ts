91002acdc1cecb8faa2895682ab7e3516e91bfb4
Igorth <igordiasth@gmail.com>
2024-03-04 19:44:13 -0500

    feat: add middleware to validate the errors

--- ts-api-restful/src/router.ts
+++ ts-api-restful/src/router.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { createMovie } from './controllers/movieControllers';
 
+// Validation
+import { validate } from './middleware/handleValidation';
+
 const router = Router();
 
 export default router
   .get('/test', (req: Request, res: Response) => {
     res.status(200).json({ msg: 'API successful' });
   })
-  .post('/movie', createMovie);
+  .post('/movie', validate, createMovie);
