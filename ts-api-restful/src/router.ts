c7032fa5638bceb305230ea4fae4c0b28e9358bd
Igorth <igordiasth@gmail.com>
2024-03-04 19:12:45 -0500

    feat: add movie Controller

--- ts-api-restful/src/router.ts
+++ ts-api-restful/src/router.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from 'express';
+import { createMovie } from './controllers/movieControllers';
 
 const router = Router();
 
-export default router.get('/test', (req: Request, res: Response) => {
-  res.status(200).json({ msg: 'API successful' });
-});
+export default router
+  .get('/test', (req: Request, res: Response) => {
+    res.status(200).json({ msg: 'API successful' });
+  })
+  .post('/movie', createMovie);
